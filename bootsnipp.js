1d1a95e3064c909f130c0b902c3eae52b3831020
Cristina Gallego <cristina@ubuntu>
2017-02-07 11:39:23 +0100

    rm

--- bootsnipp.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $('.material-button-toggle').click(function () {
-        $(this).toggleClass('open');
-        $('.option').toggleClass('scale-on');
-    });
-});
